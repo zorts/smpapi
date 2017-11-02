7c501d49d3be81916918a7394ce45a940cd44f29
Jerry Callen <jcallen@rocketsoftware.com>
2017-11-02 10:45:05 -0400

    use pragma packed for structs

--- smpapi.h
+++ smpapi.h
@@ -1,11 +1,18 @@
 #ifndef _SMPAPI_H_
 #define _SMPAPI_H_
 
+#if defined(__MVS__) && (defined (__IBMC__) || defined (__IBMCPP__))
+/* Structure packing */
+#define RKT_PRAGMA_PACK  _Pragma ( "pack(packed)" )
+#define RKT_PRAGMA_PACK_RESET  _Pragma ( "pack(reset)" )
+#endif
+
 typedef void APIPGM();
 #pragma linkage(APIPGM,OS)
 typedef void CFUNC();
 
-typedef _Packed struct QUERY_PARMS
+RKT_PRAGMA_PACK
+typedef struct QUERY_PARMS
 {
   char *csi;
   long csilen;
@@ -19,7 +26,7 @@ typedef _Packed struct QUERY_PARMS
   long filterlen;
 } QUERY_PARMS, * P_QUERY_PARMS;
 
-typedef _Packed struct API_Version
+typedef struct API_Version
 {
   char apiver[2];
   char apirel[2];
@@ -27,41 +34,43 @@ typedef _Packed struct API_Version
   char apiptf[2];
 } API_VERSION, * P_API_VERSION;
 
-typedef _Packed struct CSI_ENTRY
+typedef struct CSI_ENTRY
 {
-  _Packed struct CSI_ENTRY *next;
-  _Packed struct SUBENTRY *subentries;
+  struct CSI_ENTRY *next;
+  struct SUBENTRY *subentries;
   char entryname[8];
   char zonename[7];
 } CSI_ENTRY, * P_CSI_ENTRY;
 
-typedef _Packed struct ENTRY_LIST
+typedef struct ENTRY_LIST
 {
-  _Packed struct ENTRY_LIST *next;
-  _Packed struct CSI_ENTRY *entries;
+  struct ENTRY_LIST *next;
+  struct CSI_ENTRY *entries;
   char type[12];
 } ENTRY_LIST, * P_ENTRY_LIST;
 
-typedef _Packed struct ITEM_LIST
+typedef struct ITEM_LIST
 {
-  _Packed struct ITEM_LIST *next;
+  struct ITEM_LIST *next;
   long datalen;
   char *data;
 } ITEM_LIST, * P_ITEM_LIST;
 
 
-typedef _Packed struct SUBENTRY
+typedef struct SUBENTRY
 {
-  _Packed struct SUBENTRY *next;
+  struct SUBENTRY *next;
   void *subentrydata;
   char type[12];
 } SUBENTRY, * P_SUBENTRY;
 
-typedef _Packed struct VER
+typedef struct VER
 {
-  _Packed struct VER *next;
-  _Packed struct SUBENTRY *verdata;
+  struct VER *next;
+  struct SUBENTRY *verdata;
   char vernum[3];
 } VER, * P_VER;
+
+RKT_PRAGMA_PACK_RESET
 
 #endif /* _SMPAPI_H_ */
